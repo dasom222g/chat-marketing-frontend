76e40e139f837ce6c3a72c7e2f4a599c3d8403a4
dasom222g <dasom228@gmail.com>
2023-09-19 19:07:31 +0900

    Add router

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,19 +1,30 @@
 import React from 'react'
 import Button from '../components/Button'
 import Title from '../components/Title'
+import { useNavigate } from 'react-router-dom'
 
 const Home = (): JSX.Element => {
+  // logic
+  const history = useNavigate()
+
+  const handleClick = (): void => {
+    console.log('Click')
+    history('/user-info')
+  }
+
+  // view
   return (
-    <div className="w-full h-full overflow-hidden">
-      <div className="w-168 h-168 rounded-full bg-date-pink-500 fixed -z-10 -left-60 -top-56"></div>
-      <div className="fixed top-1/2 transform -translate-y-1/2 -z-10">
+    <div className="w-full h-full px-6 break-keep overflow-auto">
+      <i className="w-168 h-168 rounded-full bg-date-pink-500 fixed -z-10 -left-60 -top-56"></i>
+      <div className="fixed left-0 top-1/2 transform -translate-y-1/2 -z-10">
         <img src="./images/hero.png" alt="hero" />
       </div>
       <div className="h-full flex flex-col">
-        <Title />
-        <div className="mt-auto w-full flex justify-center">
-          <Button />
-        </div>
+        <Title
+          mainTitle={'소개팅 1초전'}
+          subTitle="소개팅 전, 어떤 얘기를 해야되나 고민되시나요? 미리 연습하고 가보세요!"
+        />
+        <Button onClick={handleClick} />
       </div>
     </div>
   )
