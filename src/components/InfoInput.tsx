bd242c658a1fcbd785d60abb0cd590a4995e16b7
dasom222g <dasom228@gmail.com>
2023-09-29 21:57:42 +0900

    Set Input component

--- src/components/InfoInput.tsx
+++ src/components/InfoInput.tsx
@@ -1,13 +1,22 @@
-import React, { FC } from 'react'
+import React, { FC, useState } from 'react'
 import { InfoContentType } from '../lib/types'
 
 interface InfoInputProps {
   content: InfoContentType
+  onChange: (resultData: InfoContentType) => void
 }
 
-const InfoInput: FC<InfoInputProps> = ({ content }): JSX.Element => {
+const InfoInput: FC<InfoInputProps> = ({ content, onChange }): JSX.Element => {
   // logic
   const { label, text, placeholder } = content
+
+  const [value, setValue] = useState('')
+
+  const handleChange = (event: React.ChangeEvent<HTMLInputElement>): void => {
+    const data = label === 'mbti' ? event.target.value.toUpperCase() : event.target.value
+    setValue(data)
+    onChange({ ...content, value: data })
+  }
 
   // view
   return (
@@ -20,10 +29,12 @@ const InfoInput: FC<InfoInputProps> = ({ content }): JSX.Element => {
           {text}
         </label>
         <input
-          type="text"
+          type={label === 'age' ? 'number' : 'text'}
           id={label}
           name={label}
           placeholder={placeholder}
+          value={value}
+          onChange={handleChange}
           className="border placeholder-gray-400 focus:outline-none
                       focus:border-black w-full pt-4 pr-4 pb-4 pl-4 mt-2 mr-0 mb-0 ml-0 text-base block bg-white
                       border-date-gray-200 rounded-2xl placeholder:text-date-gray-200"
